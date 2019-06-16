1954d01a74b16cf470a4d5d37d933a2dfe1ee4e8
ivar henckel <iv4307he-s@student.lu.se>
2019-06-16 13:15:27 +0200

    added cards

---
+++ Cluedo2/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cluedo2
+{
+    public class Weapon : Card
+    {
+        public Weapon (string name): base(name)
+        {
+
+        }
+    }
+}
